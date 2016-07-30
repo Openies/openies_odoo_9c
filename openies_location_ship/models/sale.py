5693a890d8dd64fe0d1e008befb5a9a73be9d214
Openies <contact@Openies.com>
2016-07-30 17:20:12 +0530

    [IMP] Added License information

--- openies_location_ship/models/sale.py
+++ openies_location_ship/models/sale.py
@@ -1,4 +1,24 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#    Copyright (C) 2014-2016 Openies Services(<http://openies.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 from openerp import api, fields, models, _
 from openerp.tools.translate import _
@@ -32,15 +52,6 @@ class StockMove(models.Model):
         else:
             return super(StockMove, self).create(vals)
         return super(StockMove, self).create(vals)
-
-# class ProcurementOrder(models.Model):
-#     _inherit = "procurement.order"
-#
-#     @api.model
-#     def create(self, vals):
-#         print "\n\nvallssssssss----procureeeeee", vals
-#         res = super(ProcurementOrder, self).create(vals)
-#         return res
 
 
 class ProductProduct(models.Model):
